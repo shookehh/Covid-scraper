fca300f58486775a604241e1a93383dbcbd9d06f
mihkuno <caindayjoeninyo@gmail.com>
2022-07-26 08:11:58 +0800

    extract string values

--- tracker.py
+++ tracker.py
@@ -1,6 +1,6 @@
 from time import sleep
-from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -8,18 +8,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = ChromeDriverManager().install()
 driver = Chrome(service=Service(driver))
 
-# target api
+# https://doh.gov.ph/covid19tracker
+# bed occupancy 
+# number of facilities
+# region/province scope
+# equipment availability 
+# occupied and vacancies
+
+# target secondhand source
 driver.get('https://www.worldometers.info/coronavirus/country/philippines/')
 sleep(1)
 
 # extract data
-totalDeath = driver.find_element(By.XPATH, "//div[@class='content-inner']//div[5]").text
-totalCases = driver.find_element(By.XPATH, "//div[@class='content-inner']//div[4]").text
-totalRecov = driver.find_element(By.XPATH, "//div[@class='content-inner']//div[6]").text
-miscUpdate = driver.find_element(By.XPATH, "//div[@id='news_block']").text
+tcases = driver.find_element(By.XPATH, "//div[@class='content-inner']//div[4]").text[19:]
+tdeath = driver.find_element(By.XPATH, "//div[@class='content-inner']//div[5]").text[8:]
+trecov = driver.find_element(By.XPATH, "//div[@class='content-inner']//div[6]").text[11:]
+mupdat = driver.find_element(By.XPATH, "//div[@id='news_block']").text
 
-print(f"Cases: {totalCases}")
-print(f"Death: {totalDeath}")
-print(f"Recov: {totalRecov}")
-print(f"Updat: {miscUpdate}")
+# updates
+ccdate = mupdat[12:19]
+ncases = mupdat[34:39]
+ndeath = mupdat[54:56]
+
+print(f"Cases: {tcases}") # total cases
+print(f"Death: {tdeath}") # total death
+print(f"Recov: {trecov}") # total recov
+print(f"Curnt: {ccdate}") # latest date  
+print(f"Newca: {ncases}") # latest cases
+print(f"Newde: {ndeath}") # latest death
 print("__________________\n")
